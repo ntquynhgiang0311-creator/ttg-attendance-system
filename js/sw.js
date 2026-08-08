464b6cd468848ebd0bab26142e84192d00806e4d
Toan Thinh Group <297052550+ntquynhgiang0311-creator@users.noreply.github.com>
2026-08-08 11:25:10 +0700

    disable service worker cache and bust admin assets

--- js/sw.js
+++ js/sw.js
@@ -1,15 +1,25 @@
-self.addEventListener(
+self.addEventListener("install", function(event) {
+    self.skipWaiting();
+});
 
-'install',
+self.addEventListener("activate", function(event) {
+    event.waitUntil(
+        caches.keys()
+            .then(function(cacheNames) {
+                return Promise.all(
+                    cacheNames.map(function(cacheName) {
+                        return caches.delete(cacheName);
+                    })
+                );
+            })
+            .then(function() {
+                return self.clients.claim();
+            })
+    );
+});
 
-()=>{
-
-console.log(
-
-'SW installed'
-
-);
-
-}
-
-);
+self.addEventListener("fetch", function(event) {
+    event.respondWith(
+        fetch(event.request)
+    );
+});
